b879d79a0b5369eafa095ba782a148b27889f8c0
bokyeong <wntjdals0412@gmail.com>
2024-01-26 16:30:58 +0900

    enhance : 제목 추가

--- backend/src/sendEmail.js
+++ backend/src/sendEmail.js
@@ -9,7 +9,7 @@ const sendEmail = async () => {
 
     const mailOptions = {
         to: process.env.RECEIVE_USER,
-        subject: "GIT PUSH NOTIFICATION", // 제목
+        subject: process.env.EMAIL_SUBJECT, // 제목
         text : "git push 알림입니다.",
         html: `<h1>GIT PUSH NOTOFICATION</h1><p>git push가 성공하였습니다!</p>`,
         // attachments: [
