e801fd10e4568383522ea1bb718b7f43f06ad0bf
bokyeong <wntjdals0412@gmail.com>
2024-01-26 16:00:58 +0900

    implement: push, pr때 이메일 보내기

---
+++ backend/src/sendEmail.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async () => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        //secure: true, // use TLS
+        auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_PASSWORD },
+    });
+
+    const mailOptions = {
+        to: process.env.RECEIVE_USER,
+        subject: "GIT PUSH NOTIFICATION", // 제목
+        text : "git push 알림입니다.",
+        html: `<h1>GIT PUSH NOTOFICATION</h1><p>git push가 성공하였습니다!</p>`,
+        // attachments: [
+        //
+        // ],
+    };
+
+    await transporter.sendMail(mailOptions);
+};
+
+sendEmail();
